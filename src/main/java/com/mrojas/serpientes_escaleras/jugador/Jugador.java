1bb6b71615af6faaa0579de3d07e62839d73925e
Manuel Rojas <manuerjs70@gmail.com>
2021-06-25 21:56:20 -0600

    Cree primeras clases y paquetes

---
+++ src/main/java/com/mrojas/serpientes_escaleras/jugador/Jugador.java
@@ -0,0 +1,9 @@
+package com.mrojas.serpientes_escaleras.jugador;
+
+/**
+ *
+ * @author Manu
+ */
+public class Jugador {
+    
+}
---
+++ src/main/java/com/mrojas/serpientes_escaleras/principal/Principal.java
@@ -0,0 +1,13 @@
+package com.mrojas.serpientes_escaleras.principal;
+
+/**
+ * Esta clase contiene el main para que la aplicación pueda ejecutarse
+ * 
+ * @author Manu
+ */
+public class Principal {
+    public static void main(String[] args) {
+        Principal p = new Principal();
+        System.out.println("Hola mundo!!!");
+    }
+}
